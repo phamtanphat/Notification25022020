171b3900e54b3d891b109ef8f3a9ad52d68fb811
phatdroid94 <phatdroid94@gmail.com>
2020-05-05 20:26:09 +0700

    start

--- app/src/main/java/com/example/notification25022020/MainActivity.java
+++ app/src/main/java/com/example/notification25022020/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.notification25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnTao,mBtnDong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnDong = findViewById(R.id.buttonDongNotification);
+        mBtnTao = findViewById(R.id.buttonTaoNotification);
+        // 1 : Giao dien notifcation : NotificationCompat.Builder
+        // 2 : PendingIntent : Sau khi xu ly xong muon ket noi voi intent de xu ly cho viec gi
+        // 3 : Quan ly tat ca notification : NotificationManager
+
+        mBtnTao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotification();
+            }
+        });
     }
+
+    private void createNotification() {
+
+    }
+
 }
